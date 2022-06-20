9a78d55267cef34a2d119abe1c16364dc56d8864
Christopher Campbell <campbellcl@mac.com>
2022-06-19 22:47:39 -0400

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState(`
+  # Welcome to my simple markdown previewer!
+  ## You can do a lot of things, including headings like this.
+  [Here's a link to my GitHub page](https://github.com/clccode)
+  \`<p>You can also insert code like this</p>\`
+  Here's an example of a larger code block:
+  \`\`\`
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+You can make lists too:
+  - Wake up
+  - Feed the cat
+  - Code
+  You might want to add blockquotes like this one:
+  > I have a dream!
+  You can also add images:
+  ![code image](https://live.staticflickr.com/822/40238552784_c48d2029a2_n.jpg)
+  **It's easy to add bold text...**
+  *...and text in italics too.*
+  Need to make an edit? Use ~~strikethrough~~!
+  `);
+
+  marked.setOptions({
+    breaks: true
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mdown-preview">
+        <h1>Markdown Previewer</h1>
+        <textarea 
+        id="editor"
+        onChange={(event) => {
+          setText(event.target.value)
+        }}
+        value={text}
+        ></textarea>
+      </div>
+        <div className='preview-pane'>
+        <div 
+          id="preview"
+          dangerouslySetInnerHTML={{
+            __html: marked(text),
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
